00b18f91f231c79e828beee3de50c6cdda93dd3e
Jonathan Le <jle@vandal.io>
2014-02-11 21:11:12 -0800

    Declare ACC flags constants
    - Add convenience methods for a few accessFlag checks
    
    - Rename hasSuperClass() to isSubclass()

--- src/main/java/com/vandalsoftware/tools/classfile/ClassInfo.java
+++ src/main/java/com/vandalsoftware/tools/classfile/ClassInfo.java
@@ -7,6 +7,38 @@ import java.util.Collection;
  */
 public class ClassInfo {
     static final String JAVA_LANG_OBJECT = "java.lang.Object";
+    /**
+     * Declared public; may be accessed from outside its package.
+     */
+    private static final int ACC_PUBLIC = 0x0001;
+    /**
+     * Declared final; no subclasses allowed.
+     */
+    private static final int ACC_FINAL = 0x0010;
+    /**
+     * Treat superclass methods specially when invoked by the invokespecial instruction.
+     */
+    private static final int ACC_SUPER = 0x0020;
+    /**
+     * Is an interface, not a class.
+     */
+    private static final int ACC_INTERFACE = 0x0200;
+    /**
+     * Declared abstract; must not be instantiated.
+     */
+    private static final int ACC_ABSTRACT = 0x0400;
+    /**
+     * Declared synthetic; not present in the source code.
+     */
+    private static final int ACC_SYNTHETIC = 0x1000;
+    /**
+     * Declared as an annotation type.
+     */
+    private static final int ACC_ANNOTATION = 0x2000;
+    /**
+     * Declared as an enum type.
+     */
+    private static final int ACC_ENUM = 0x4000;
     public final int minorVersion;
     public final int majorVersion;
     public final String[] strings;
@@ -43,9 +75,25 @@ public class ClassInfo {
     }
 
     /**
-     * @return true if this class' superclass is not java.lang.Object.
+     * @return true if this class' superclass is not {@link java.lang.Object}.
      */
-    public boolean hasSuperClass() {
+    public boolean isSubclass() {
         return !JAVA_LANG_OBJECT.equals(this.superClassName);
+    }
+
+    public boolean isPublic() {
+        return (this.accessFlags & ACC_PUBLIC) != 0;
+    }
+
+    public boolean isFinal() {
+        return (this.accessFlags & ACC_FINAL) != 0;
+    }
+
+    public boolean isInterface() {
+        return (this.accessFlags & ACC_INTERFACE) != 0;
+    }
+
+    public boolean isAbstract() {
+        return (this.accessFlags & ACC_ABSTRACT) != 0;
     }
 }
