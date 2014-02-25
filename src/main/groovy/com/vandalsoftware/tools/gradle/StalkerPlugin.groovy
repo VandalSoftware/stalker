c4d3ac87a4b6c8e49f9a143c2a660514d2bde806
Jonathan Le <jle@vandal.io>
2014-02-25 08:54:53 -0800

    Remove afterEvaluate closure
    Allow Stalker tasks to be added in the configuration step as the
    apply method is called instead of after the project has been
    evaluated.

--- src/main/groovy/com/vandalsoftware/tools/gradle/StalkerPlugin.groovy
+++ src/main/groovy/com/vandalsoftware/tools/gradle/StalkerPlugin.groovy
@@ -13,56 +13,54 @@ class StalkerPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         extension = project.extensions.create("stalker", StalkerExtension)
-        project.afterEvaluate {
-            Task changesTask = project.task([type: GetChangedFiles], "changes", {
-                def ref = extension.getRevision()
-                args "-r"
-                if (ref) {
-                    args ref
+        Task changesTask = project.task([type: GetChangedFiles], "changes", {
+            def ref = extension.getRevision()
+            args "-r"
+            if (ref) {
+                args ref
+            } else {
+                args "HEAD"
+            }
+            standardOutput = new ByteArrayOutputStream()
+            ext.output = {
+                String out = standardOutput.toString()
+                out.split("\n") as Set
+            }
+        })
+        Task stalkTask = project.task([type: Usages, dependsOn: changesTask], "stalk", {
+            ext.srcRoots = {
+                return extension.getSrcRoots()
+            }
+            ext.classpaths = {
+                return extension.getSrcClassPaths()
+            }
+            ext.input = {
+                return changesTask.output()
+            }
+            ext.targets = {
+                return extension.getTargetClassPaths()
+            }
+            description = "Analyze class usage"
+            group = "Analyze"
+        }) << {
+            if (extension.afterStalk) {
+                extension.afterStalk(classNames)
+            } else {
+                if (extension.standardOutput != null) {
+                    PrintStream out = new PrintStream(extension.standardOutput, true)
+                    classNames.each() {
+                        out.println(it)
+                    }
+                    out.close()
                 } else {
-                    args "HEAD"
-                }
-                standardOutput = new ByteArrayOutputStream()
-                ext.output = {
-                    String out = standardOutput.toString()
-                    out.split("\n") as Set
-                }
-            })
-            Task stalkTask = project.task([type: Usages, dependsOn: changesTask], "stalk", {
-                ext.srcRoots = {
-                    return extension.getSrcRoots()
-                }
-                ext.classpaths = {
-                    return extension.getSrcClassPaths()
-                }
-                ext.input = {
-                    return changesTask.output()
-                }
-                ext.targets = {
-                    return extension.getTargetClassPaths()
-                }
-                description = "Analyze class usage"
-                group = "Analyze"
-            }) << {
-                if (extension.afterStalk) {
-                    extension.afterStalk(classNames)
-                } else {
-                    if (extension.standardOutput != null) {
-                        PrintStream out = new PrintStream(extension.standardOutput, true)
-                        classNames.each() {
-                            out.println(it)
-                        }
-                        out.close()
-                    } else {
-                        classNames.each() {
-                            println(it);
-                        }
+                    classNames.each() {
+                        println(it);
                     }
                 }
             }
-            stalkTask.onlyIf {
-                it.checkInputs()
-            }
+        }
+        stalkTask.onlyIf {
+            it.checkInputs()
         }
     }
 }
