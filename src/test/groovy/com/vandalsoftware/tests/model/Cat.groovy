3ffc420595c4c81d951f1dd56038c55ae49edf9f
Jonathan Le <jle@vandal.io>
2014-03-14 13:53:01 -0700

    Fix bug where 'this' class is not checked for usage
    When checking the usage of a file, the class itself should be
    checked in addition to classes it uses.

---
+++ src/test/groovy/com/vandalsoftware/tests/model/Cat.groovy
@@ -0,0 +1,10 @@
+package com.vandalsoftware.tests.model
+
+import com.vandalsoftware.tests.model.Animal
+import com.vandalsoftware.tests.model.FourLegged
+
+/**
+ * @author Jonathan Le
+ */
+class Cat implements Animal, FourLegged {
+}
--- src/test/groovy/com/vandalsoftware/tools/gradle/UsagesTests.groovy
+++ src/test/groovy/com/vandalsoftware/tools/gradle/UsagesTests.groovy
@@ -33,4 +33,30 @@ class UsagesTests {
         assertTrue('UsagesTests is an affected file',
                 usages.classNames.contains("com.vandalsoftware.tools.gradle.UsagesTests"))
     }
+
+    /**
+     * Tests that there is an affected file when the input file is in both the source and the
+     * target class paths.
+     */
+    @Test
+    void testAffectedFileInSrcAndTargetClasspath() {
+        Project project = ProjectBuilder.builder().build()
+        Usages usages = project.task([type: Usages], "usages", {
+            ext.srcRoots = {
+                [new File("src/main/groovy"), new File("src/test/groovy")] as Set
+            }
+            ext.classpaths = {
+                [new File("build/classes/main"), new File("build/classes/test")] as Set
+            }
+            ext.targets = {
+                [new File("build/classes/test")] as Set
+            }
+            ext.input = {
+                ["src/test/groovy/com/vandalsoftware/tests/model/Cat.groovy"] as Set
+            }
+        }) as Usages
+        usages.execute()
+        assertTrue('Cat is an affected file',
+                usages.classNames.contains("com.vandalsoftware.tests.model.Cat"))
+    }
 }
